28b43c78a1f77984df14bad960d88375c24f781e
Punit Grewal <punit144@gmail.com>
2023-06-12 10:06:33 +0530

    [PG] Cleanup and adding gitignore

--- 3tierapp/vars.tf
+++ 3tierapp/vars.tf
@@ -10,17 +10,19 @@ variable "subnet_cidrs" {
 }
 variable "security_group_name" {}
 variable "security_rules" {
-  type        = list(object({
-    name                      = string
-    priority                  = number
-    direction                 = string
-    access                    = string
-    protocol                  = string
-    source_port_range         = string
-    destination_port_range    = string
-    source_address_prefix     = string
-    destination_address_prefix = string
-  }))
+  type        = list(object(
+    {
+      name                       = string
+      priority                   = number
+      direction                  = string
+      access                     = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    }
+  ))
 }
 variable "virtual_machine_name" {}
 variable "vm_size" {}
